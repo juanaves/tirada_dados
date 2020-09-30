e663564997670bbffbad31c9213a5c6a52473354
juanaves <juanaves@gmail.com>
2020-09-30 06:11:59 +0200

    commit1

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'head.php';
+echo' Realiza un programa que vaya generando números entre 1 y 50, HASTA que se genere el numero 1, en cuyo caso se
+    terminará de generar numeros.<br>
+Deberemos calcular:
+<ol><li>Cuantos números se han introducido</li><li>La media de los impares</li> <li>El mayor de los pares</li></ol>';
+
+
+
+ include 'pie.php';

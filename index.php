64d22de64568f157356cbecb7225e6160cc8d273
juanaves <juanaves@gmail.com>
2020-09-30 06:46:48 +0200

    55

--- index.php
+++ index.php
@@ -11,23 +11,23 @@ echo 'Escribe un script PHP que realice la simulación de lanzar 5 dados y
  echo' Cuando termine la simulacion de la tirada me debera aparecer la suma de los
  numeros que han aparecido en los dados';
  $suma=0;
- /*for ($i=0;$i<5;$i++)
+ for ($i=0;$i<5;$i++)
  {
      $numero=rand(1,6);
      
      switch($numero)
      {
-         case 1:echo'<img src="imagenes_dados/uno.jpg" alt="uno" width="128" height="128">';break;
-         case 2:echo'<img src="imagenes_dados/dos.jpg" alt="dos" width="128" height="128">';break;
-         case 3:echo'<img src="imagenes_dados/tres.jpg" alt="tres" width="128" height="128">';break;
-         case 4:echo'<img src="imagenes_dados/cuatro.jpg" alt="cuatro" width="128" height="128">';break;
-         case 5:echo'<img src="imagenes_dados/cinco.jpg" alt="cinco" width="128" height="128">';break;
-         case 6:echo'<img src="imagenes_dados/seis.jpg" alt="seis" width="128" height="128">';break;
+         case 1:echo'<img src="imagenes/uno.jpg" alt="uno" width="128" height="128">';break;
+         case 2:echo'<img src="imagenes/dos.jpg" alt="dos" width="128" height="128">';break;
+         case 3:echo'<img src="imagenes/tres.jpg" alt="tres" width="128" height="128">';break;
+         case 4:echo'<img src="imagenes/cuatro.jpg" alt="cuatro" width="128" height="128">';break;
+         case 5:echo'<img src="imagenes/cinco.jpg" alt="cinco" width="128" height="128">';break;
+         case 6:echo'<img src="imagenes/seis.jpg" alt="seis" width="128" height="128">';break;
 
     }
     sleep(1);
     $suma+=$numero;
 
 
-}*/
+}
 echo 'La suma es : ',$suma;
